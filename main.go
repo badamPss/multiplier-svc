6652cd037fcb3b5b8e917cccc5eaf88afb9197a8
badamPss <ignatvaschilo@yandex.ru>
2025-09-08 01:14:32 +0300

    [fix] error with rtp

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	flag.Float64Var(&rtp, "rtp", 0, "rtp")
 	flag.Parse()
 
-	if rtp <= 0 && rtp > 1 {
+	if rtp <= 0 || rtp > 1 {
 		log.Fatal("error: rtp should be between 0 and 1")
 	}
 
